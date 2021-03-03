81d211f3bdb8fd66c0dc0c3a1b9ebcdc2890db0e
Khinshan Khan <khanshan501@gmail.com>
2021-03-03 16:10:00 -0500

    added conceptual db interaction

--- data-scraper/app.py
+++ data-scraper/app.py
@@ -4,8 +4,10 @@ app.py
 Heart of data scraper.
 This will initialize any relevant tables and fill them with speech data.
 """
-
 import db
 
 if __name__ == "__main__":
-    db.ensure_scrape_tables()
+    try:
+        db.ensure_scrape_tables()
+    finally:
+        db.cleanup()
--- data-scraper/db.py
+++ data-scraper/db.py
@@ -3,11 +3,27 @@ db.py
 
 Methods to interact with database for data scraper.
 """
+import pathlib
+import sqlite3
+
+BASE_DIR = pathlib.Path(__file__).parent.absolute()
+DB_PATH = BASE_DIR.joinpath('..', 'database.db')
+
+connection = sqlite3.connect(DB_PATH)
 
 
 def ensure_scrape_tables():
     print("db!")
+    print(connection.total_changes)
+
+
+def cleanup():
+    print("data scraper closed connection to database.db")
+    connection.close()
 
 
 if __name__ == "__main__":
-    ensure_scrape_tables()
+    try:
+        ensure_scrape_tables()
+    finally:
+        cleanup()
