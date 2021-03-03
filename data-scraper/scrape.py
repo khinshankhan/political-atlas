c0350d91132c9bc5d9718282185345fbb23f39da
adeeburrahman <57969730+adeeburrahman@users.noreply.github.com>
2021-03-03 15:28:34 -0500

    obtain each speech name

--- data-scraper/scrape.py
+++ data-scraper/scrape.py
@@ -4,6 +4,6 @@ import bs4
 listhtml = requests.get('https://millercenter.org/the-presidency/presidential-speeches').text
 listsoup = bs4.BeautifulSoup(listhtml, features='html.parser')
 
-for i in listsoup.findAll(attrs={'class':'views-field-title'}):
-    print(i)
+for speech in listsoup.findAll(attrs={'class':'views-field-title'})[:5]:
+    print("Title:", speech.text)
     print()
