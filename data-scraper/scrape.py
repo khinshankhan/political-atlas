068fe5172f53c63a19d4c1dace852a4ef9ee31f4
adeeburrahman <57969730+adeeburrahman@users.noreply.github.com>
2021-03-03 15:25:58 -0500

    iterate over each speech available

--- data-scraper/scrape.py
+++ data-scraper/scrape.py
@@ -4,4 +4,6 @@ import bs4
 listhtml = requests.get('https://millercenter.org/the-presidency/presidential-speeches').text
 listsoup = bs4.BeautifulSoup(listhtml, features='html.parser')
 
-print(listsoup)
+for i in listsoup.findAll(attrs={'class':'views-field-title'}):
+    print(i)
+    print()
