a83a72e679c7c425e2f09996064e5b44655231d3
adeeburrahman <57969730+adeeburrahman@users.noreply.github.com>
2021-03-03 15:36:26 -0500

    obtain each speech as bs4 object

--- data-scraper/scrape.py
+++ data-scraper/scrape.py
@@ -1,10 +1,21 @@
 import requests
 import bs4
 
-listhtml = requests.get('https://millercenter.org/the-presidency/presidential-speeches').text
+baselink = 'http://millercenter.org'
+
+listlink = 'https://millercenter.org/the-presidency/presidential-speeches'
+listhtml = requests.get(listlink).text
 listsoup = bs4.BeautifulSoup(listhtml, features='html.parser')
 
-for speech in listsoup.findAll(attrs={'class':'views-field-title'})[:5]:
+for speech in listsoup.findAll(attrs={'class':'views-field-title'})[:3]:
     print("Title:", speech.text)
-    print("Speech Link:", speech.find('a'))
+
+    speechlink = speech.find('a').get('href')
+    print("Speech Link:", speechlink)
+
+    speechhtml = requests.get(baselink + speechlink).text
+    speechsoup = bs4.BeautifulSoup(speechhtml, features='html.parser')
+
+    print(speechsoup)
+
     print()
