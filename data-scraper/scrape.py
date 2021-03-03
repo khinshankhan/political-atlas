5d83363ceadf045dbd0f4e7a2ada54b36e05f22a
adeeburrahman <57969730+adeeburrahman@users.noreply.github.com>
2021-03-03 15:43:32 -0500

    get video link

--- data-scraper/scrape.py
+++ data-scraper/scrape.py
@@ -8,14 +8,14 @@ listhtml = requests.get(listlink).text
 listsoup = bs4.BeautifulSoup(listhtml, features='html.parser')
 
 for speech in listsoup.findAll(attrs={'class':'views-field-title'})[:3]:
-    print("Title:", speech.text)
+    print('Title:', speech.text)
 
     speechlink = speech.find('a').get('href')
-    print("Speech Link:", speechlink)
+    print('Speech Link:', speechlink)
 
     speechhtml = requests.get(baselink + speechlink).text
     speechsoup = bs4.BeautifulSoup(speechhtml, features='html.parser')
 
-    print(speechsoup)
+    print('Video Link:', speechsoup.find(attrs={'class': 'download-trigger full-video'}).get('href'))
 
     print()
