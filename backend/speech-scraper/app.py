882e185f50973f6fe31b06c12c7bec05b3d0dc7c
Khinshan Khan <khanshan501@gmail.com>
2021-03-08 16:37:24 -0500

    added db insertions, need to normalize text of metacharacters

--- backend/speech-scraper/app.py
+++ backend/speech-scraper/app.py
@@ -4,13 +4,22 @@ app.py
 Heart of data scraper.
 This will initialize any relevant tables and fill them with speech data.
 """
+import scrape
 import db
 
 if __name__ == "__main__":
     try:
         db.ensure_scrape_tables()
-        # TODO: remove this example once the scrape uses the add query
-        # db.add_scrape("John Doe", "Inauguration", "foo", "bar", "baz",
-        #               "date", "cool speech", "hello")
+        for speech in scrape.millerscrape():
+            db.add_scrape(
+                speech['politician'],
+                speech['title'],
+                speech['speech-link'],
+                speech['video-link'],
+                speech['audio-link'],
+                speech['date'],
+                speech['description'],
+                speech['transcript']
+            )
     finally:
         db.cleanup()
