9035c588cc8b6ed7639daeb47a1718c8d858fbe6
Khinshan Khan <khanshan501@gmail.com>
2021-03-15 16:50:43 -0400

    rudimentary footer

--- frontend/website/src/components/Layout/Footer.jsx
+++ frontend/website/src/components/Layout/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Footer = () => {
-  return <p>Footer</p>;
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+    marginTop: "auto",
+    backgroundColor: "gray",
+  },
+}));
+
+const Footer = ({ buttons, links }) => {
+  const classes = useStyles();
+
+  return <div className={classes.root}>Footer</div>;
 };
 
 export default Footer;
