ec1e3a3c65f7fea3450076ad105f0d98f2a2ddc1
adeeburrahman <57969730+adeeburrahman@users.noreply.github.com>
2021-03-15 16:05:43 -0400

    remove temporary fix in scrape insertion

--- backend/speech-scraper/app.py
+++ backend/speech-scraper/app.py
@@ -18,10 +18,6 @@ if __name__ == "__main__":
     try:
         db.ensure_scrape_tables()
         for speech in scrape.millerscrape():
-            # TODO: fix in actual scrape generation
-            speech['speech_link'] = speech['speech-link']
-            speech['video_link'] = speech['video-link']
-            speech['audio_link'] = speech['audio-link']
             db.add_scrape(speech)
     finally:
         db.cleanup()
