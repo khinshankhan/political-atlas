3cab6a80e24fdc57f647ad7824617c81ea74d53c
Khinshan Khan <khanshan501@gmail.com>
2021-03-17 15:14:04 -0400

    added in footer contents

--- frontend/website/src/components/Layout/Footer.jsx
+++ frontend/website/src/components/Layout/Footer.jsx
@@ -7,13 +7,32 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(3, 2),
     marginTop: "auto",
     backgroundColor: "gray",
+    textAlign: "center",
+    color: "white",
+  },
+  nolink: {
+    color: "white",
   },
 }));
 
 const Footer = ({ buttons, links }) => {
   const classes = useStyles();
 
-  return <div className={classes.root}>Footer</div>;
+  const startYear = 2021;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className={classes.root}>
+      <a
+        href="https://github.com/kkhan01/political-atlas/blob/main/LICENSE"
+        target="_blank"
+        rel="noreferrer"
+        className={classes.nolink}
+      >
+        Political Atlas &copy; {startYear} {currentYear !== startYear && `- ${currentYear}`}
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
