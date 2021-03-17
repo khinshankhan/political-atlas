1c61683b6141764678cbc1f4792aedd741b86d5a
Khinshan Khan <khanshan501@gmail.com>
2021-03-17 17:03:47 -0400

    added more validation for id and planned out positions of components

--- frontend/website/src/pages/speech.jsx
+++ frontend/website/src/pages/speech.jsx
@@ -1,16 +1,57 @@
-import React from "react";
-import { useQueryParam, StringParam } from "use-query-params";
+import React, { useState, useEffect } from "react";
+import { useQueryParam, NumberParam } from "use-query-params";
+
+import { getSpeechMeta } from "src/api/Server";
 
 import Layout from "src/components/Layout";
 
 const Speech = () => {
-  const [id] = useQueryParam("id", StringParam);
+  const [id] = useQueryParam("id", NumberParam);
+  const [validId] = useState(id != null && !Number.isNaN(id));
+  const [speechMeta, setSpeechMeta] = useState(null);
 
-  if (id == null) {
-    return <Layout>bad id</Layout>;
+  useEffect(() => {
+    const setup = async () => {
+      const fetchedSpeechMeta = await getSpeechMeta(id);
+      setSpeechMeta((prev) => fetchedSpeechMeta);
+      // TODO: split up other api calls into use effects relying on speechMetaData change
+    };
+
+    if (validId) {
+      setup();
+    }
+  }, [validId, id]);
+
+  if (!validId) {
+    return (
+      <Layout>
+        <center>Invalid or no id provided.</center>
+      </Layout>
+    );
   }
 
-  return <Layout>Speech {id}</Layout>;
+  // TODO: add case for failed fetch
+  if (speechMeta == null) {
+    return <Layout>Loading.</Layout>;
+  }
+
+  return (
+    <Layout>
+      <center>
+        [title]
+        <br />
+        [description]
+        <br />
+        [video]
+        <br />
+        [legend]
+        <br />
+        [charts]
+        <br />
+        [text with emotions]
+      </center>
+    </Layout>
+  );
 };
 
 export default Speech;
