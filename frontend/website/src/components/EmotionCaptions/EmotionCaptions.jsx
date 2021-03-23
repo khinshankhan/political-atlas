fcd654ef6572f8348f7bcdf9c2867e1866583858
Khinshan Khan <khanshan501@gmail.com>
2021-03-23 16:48:03 -0400

    figured out radio switches for emotion selected

--- frontend/website/src/components/EmotionCaptions/EmotionCaptions.jsx
+++ frontend/website/src/components/EmotionCaptions/EmotionCaptions.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Switch from "@material-ui/core/Switch";
+
+import { emotionMap, sortedEmotions } from "src/utils/emotions";
+import { capitalize } from "src/utils/utils";
+
 // TODO: figure out actual coloring per sentence
 // TODO: look into on hovers for the text
 
@@ -19,6 +28,10 @@ const useStyles = makeStyles((theme) => ({
 }));
 const EmotionCaptions = ({ sentences }) => {
   const classes = useStyles();
+
+  const [selectedEmotion, setSelectedEmotion] = useState(sortedEmotions[0]);
+  const updateEmotionOnChange = (e) => setSelectedEmotion(e.target.name);
+
   const text = sentences.map(({ text }) => text).join(" ");
 
   return (
@@ -27,18 +40,25 @@ const EmotionCaptions = ({ sentences }) => {
         <Grid item xs={2}>
           <Paper className={classes.paper}>
             <Grid container spacing={1}>
-              {[...Array(7).keys()].map((i) => (
-                <Grid item xs={12}>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    component="span"
-                    key={i}
-                  >
-                    Enotion {i}
-                  </Button>
-                </Grid>
-              ))}
+              <Grid item xs={12}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">Choose Emotion</FormLabel>
+                  <FormGroup>
+                    {sortedEmotions.map((emotion, i) => (
+                      <FormControlLabel
+                        control={
+                          <Switch
+                            checked={emotion === selectedEmotion}
+                            onChange={updateEmotionOnChange}
+                            name={emotion}
+                          />
+                        }
+                        label={capitalize(emotion)}
+                      />
+                    ))}
+                  </FormGroup>
+                </FormControl>
+              </Grid>
             </Grid>
           </Paper>
         </Grid>
