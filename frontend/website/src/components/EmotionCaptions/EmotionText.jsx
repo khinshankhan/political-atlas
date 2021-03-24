d3b2f51e65bcdc89a113f6161b10c8b63426f608
Khinshan Khan <khanshan501@gmail.com>
2021-03-24 16:22:46 -0400

    able to visualize sentence emotions via colors, need to figure out confidence interval logic

--- frontend/website/src/components/EmotionCaptions/EmotionText.jsx
+++ frontend/website/src/components/EmotionCaptions/EmotionText.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
-const EmotionText = ({ sentences }) => {
-  return <>{sentences.map(({ text }) => text).join(" ")}</>;
+import Typography from "@material-ui/core/Typography";
+
+const EmotionText = ({ sentences, emotionObj }) => {
+  // this is for a special case
+  // since the ibm emotions will be empty when the emotion is neutral
+  const fallback = emotionObj.ibm.includes("neutral");
+  const colorEmotionP = (tones) => {
+    if (fallback) {
+      return tones.length === 0;
+    }
+    return tones.some(({ tone_id }) => emotionObj.ibm.includes(tone_id));
+  };
+
+  return (
+    <Typography variant="body1" gutterBottom>
+      {sentences.map(({ tones, text }, i) => {
+        if (colorEmotionP(tones)) {
+          return (
+            <span
+              key={i}
+              style={{
+                backgroundColor: emotionObj.color,
+              }}
+            >
+              {text}
+            </span>
+          );
+        }
+
+        return <span key={i}>{text}</span>;
+      })}
+    </Typography>
+  );
 };
 
 export default EmotionText;
