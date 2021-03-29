bf57554899b4f76284e7f9fbc04f09aafff49cd5
Khinshan Khan <khanshan501@gmail.com>
2021-03-29 15:59:38 -0400

    simple card format done

--- frontend/website/src/components/SpeechList/SpeechCard.jsx
+++ frontend/website/src/components/SpeechList/SpeechCard.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 1440,
+  },
+  media: {
+    height: 140,
+  },
+  right: {
+    float: "right",
+  },
+});
+
 const SpeechCard = ({ speech }) => {
-  return <>speech</>;
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {speech.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {speech.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.right}>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          View Speech
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default SpeechCard;
