aa4b2e0af01ce5d012f96db2a38914f9b756bfd4
Khinshan Khan <khanshan501@gmail.com>
2021-04-03 15:40:50 -0400

    added logo as favicon

--- frontend/website/config/plugins.js
+++ frontend/website/config/plugins.js
@@ -2,4 +2,19 @@ module.exports = [
   "gatsby-plugin-root-import",
   "gatsby-theme-material-ui",
   "gatsby-plugin-use-query-params",
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "assets",
+      path: `${__dirname}/../src/assets`,
+    },
+  },
+  {
+    resolve: "gatsby-plugin-manifest",
+    options: {
+      start_url: "/",
+      icon: "src/assets/logo.png",
+      cache_busting_mode: "none",
+    },
+  },
 ];
