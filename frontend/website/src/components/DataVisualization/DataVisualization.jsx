83504e04535b583e3493c0157354915834d85710
Khinshan Khan <khanshan501@gmail.com>
2021-04-04 18:15:18 -0400

    moving around data transformations

--- frontend/website/src/components/DataVisualization/DataVisualization.jsx
+++ frontend/website/src/components/DataVisualization/DataVisualization.jsx
@@ -2,51 +2,10 @@ import React, { useState, useEffect } from "react";
 import BarChart from "./BarChart";
 
 const DataVisualization = ({ ibm, da }) => {
-  const [dataIBM, setIBM] = useState(null);
-  const [dataDA, setDA] = useState(null);
-
-  useEffect(() => {
-    if (ibm && Object.keys(ibm).length !== 0) {
-      const temp = ibm.sentences_tone.reduce((stored, current) => {
-        const tones = current.tones;
-        if (tones === []) return stored;
-        const emotions = tones.reduce((innerstored, innercurrent) => {
-          const emotion = innercurrent.tone_id;
-          return [...innerstored, emotion];
-        }, []);
-        return [...stored, ...emotions];
-      }, []);
-      const cleanedData = temp.reduce((stored, current) => {
-        return { ...stored, [current]: stored[current] + 1 || 1 };
-      }, {});
-      const dataIBM = Object.entries(cleanedData).map(([emotion, value]) => ({
-        emotion,
-        value,
-      }));
-      setIBM(dataIBM);
-    }
-  }, [ibm]);
-
-  useEffect(() => {
-    if (da && Object.keys(da).length !== 0) {
-      const cleanedData = da.response.reduce((stored, current) => {
-        const emotion = current.emotion;
-        if (emotion === "neutral") return stored;
-        return { ...stored, [emotion]: stored[emotion] + 1 || 1 };
-      }, {});
-
-      const dataDA = Object.entries(cleanedData).map(([emotion, value]) => ({
-        emotion,
-        value,
-      }));
-      setDA(dataDA);
-    }
-  }, [da]);
-
+  console.log({ ibm, da });
   return (
     <>
-      {dataIBM != null && <BarChart data={dataIBM} />}
-      {dataDA != null && <BarChart data={dataDA} />}
+      <BarChart ibm={ibm} da={da} />
     </>
   );
 };
