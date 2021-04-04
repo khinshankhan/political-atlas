3d99979ead6e416ccc6d4c476578d7bd0f4a83b4
Khinshan Khan <khanshan501@gmail.com>
2021-04-04 19:22:23 -0400

    fixed emotions to work with newly pre-transformed data

--- frontend/website/src/components/EmotionCaptions/EmotionText.jsx
+++ frontend/website/src/components/EmotionCaptions/EmotionText.jsx
@@ -24,29 +24,16 @@ const determineColor = (color, score) => {
 const EmotionText = ({ sentences, emotion, emotionObj }) => {
   const classes = useStyles();
 
-  // this is for a special case
-  // since the ibm emotions will be empty when the emotion is neutral
-  const fallback = emotionObj.ibm.includes("neutral");
-  const colorEmotionP = (tones) => {
-    if (fallback) {
-      return tones.length === 0;
-    }
-    return tones.some(({ tone_id }) => emotionObj.ibm.includes(tone_id));
+  const findEmotion = (tones) => {
+    return tones.find(({ tone_id }) => tone_id === emotion);
   };
 
   return (
     <Typography variant="body1" gutterBottom>
       {sentences.map(({ tones, text }, i) => {
         let score = 0;
-        if (colorEmotionP(tones)) {
-          if (fallback) {
-            score = 90;
-          } else {
-            score = tones.find(({ tone_id }) =>
-              emotionObj.ibm.includes(tone_id)
-            ).score;
-          }
-        }
+        const emotionFound = findEmotion(tones);
+        if (emotionFound != null) score = emotionFound.score;
 
         const [backgroundColor, textColor] = determineColor(
           emotionObj.color,
