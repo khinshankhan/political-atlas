2b57894d1a6793d4106c5dfda3711122191ba2d3
Khinshan Khan <khanshan501@gmail.com>
2021-04-04 18:58:51 -0400

    figured out all the data transformations for uniform data

--- frontend/website/src/components/DataVisualization/DataVisualization.jsx
+++ frontend/website/src/components/DataVisualization/DataVisualization.jsx
@@ -1,38 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import BarChart from "./BarChart";
 
-import { ibmMap, daMap } from "src/utils/emotions";
+import { uniformDaData, uniformIbmData } from "src/utils/dataTransformations";
 
 const DataVisualization = ({ ibm, da }) => {
-  const [dataIbm, setIbm] = useState(null);
-  const [dataDa, setDa] = useState(null);
-
-  useEffect(() => {
-    if (ibm && Object.keys(ibm).length !== 0) {
-      console.log({ ibm });
-      setIbm({ ...ibm });
-    }
-  }, [ibm]);
-
-  useEffect(() => {
-    if (da && Object.keys(da).length !== 0) {
-      const newDa = da.response.map((daObj) => {
-        if (daMap[daObj.emotion] == null) {
-          console.log(daObj.emotion);
-        }
-        return {
-          ...daObj,
-          emotion: daMap[daObj.emotion],
-        };
-      });
-      setDa(newDa);
-    }
-  }, [da]);
-
   return (
     <>
-      <BarChart ibm={dataIbm} da={dataDa} />
+      <BarChart ibm={uniformIbmData(ibm)} da={uniformDaData(da)} />
     </>
   );
 };
