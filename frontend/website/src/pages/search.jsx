b39cbffd1525ba49d701c804dff6b3c700fd54de
Khinshan Khan <khanshan501@gmail.com>
2021-04-19 17:03:18 -0400

    started search fields

--- frontend/website/src/pages/search.jsx
+++ frontend/website/src/pages/search.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
+import DateFnsUtils from "@date-io/date-fns";
 
 import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import TextField from "@material-ui/core/TextField";
 
 import Layout from "src/components/Layout";
 import SpeechList from "src/components/SpeechList";
 
 import { getSpeechList } from "src/api/Server";
+
+import { capitalize } from "src/utils/utils";
 
 const useStyles = makeStyles((theme) => ({
   load: {
@@ -14,32 +18,69 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     alignItems: "center",
   },
+  searchinput: {
+    textAlign: "center",
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
 }));
 
 const Search = () => {
   const classes = useStyles();
 
   const [speeches, setSpeeches] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [speechesToDisplay, setSpeechesToDisplay] = useState(null);
+
+  const [input, setInput] = useState({
+    description: "",
+    transcript: "",
+    president: "",
+  });
 
   useEffect(() => {
     const setup = async () => {
       const list = await getSpeechList();
-      setSpeeches(list);
+      setSpeeches((list && list.data) || []);
+      setLoading(false);
     };
     setup();
   }, []);
 
+  const handleInputChange = (e) => {
+    setInput((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
   return (
     <Layout title="Search">
-      <br />
-      {speeches === null ? (
+      <div className={classes.searchinput}>
+        {["description", "transcript"].map((field) => (
+          <TextField
+            key={field}
+            id={field}
+            label={capitalize(field)}
+            value={input[field]}
+            onChange={handleInputChange}
+          />
+        ))}
+        <TextField
+          id="president"
+          label="President"
+          value={input.president}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      {loading && (
         <div className={classes.load}>
           <br />
           <CircularProgress size={100} />
         </div>
-      ) : (
-        <SpeechList speeches={speeches.data} />
       )}
+
+      {speechesToDisplay != null && <SpeechList speeches={speeches} />}
     </Layout>
   );
 };
