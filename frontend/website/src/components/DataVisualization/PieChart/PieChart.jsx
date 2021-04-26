e307e5dcab73269b86574dae879b3ae73ee3ab9b
Talha Rahman <39923265+TalhaR@users.noreply.github.com>
2021-04-25 23:35:10 -0400

    fixed issue with data not having all emotions

--- frontend/website/src/components/DataVisualization/PieChart/PieChart.jsx
+++ frontend/website/src/components/DataVisualization/PieChart/PieChart.jsx
@@ -4,72 +4,63 @@ import * as d3 from "d3";
 
 import "./PieChart.css";
 
-import { sortedEmotions } from "src/utils/emotions";
-import { roundUpX, roundDecimal2 } from "src/utils/utils";
-
 const PieChart = ({ data, title = "Pie Chart" }) => {
+    // const margin = { top: 20, right: 20, bottom: 60, left: 40 };
+    // const width = 600 - margin.left - margin.right;
+    // const height = 300 - margin.top - margin.bottom;
+    const width = 600;
+    const height = 300;
     const ref = useRef(null);
-    // HACK: makes hover work on chart, isn't really proper in react nor html
     const chartDivRef = useRef();
 
     useEffect(() => {
-        // TODO: move this outside and hook it into the screen size
-        let width = 450;
-        let height = 450;
-        let margin = 40;
-
         const innerRadius = 40;
         const outerRadius = 100;
+
+        const svgElement = d3.select(ref.current);
+
+        // add in chart title
+        svgElement
+            .append("g")
+            .attr("transform", `translate(${-20}, ${-height / 2 + 20})`)
+            .append("text")
+            .text(title)
+            .style("text-decoration", "underline")
+            .attr("class", "title");
 
         const createPie = d3
             .pie()
             .value((d) => d.value)
-            .sort(null);
+            .sort((a, b) => (a.emotion > b.emotion ? 1 : -1));
         const createArc = d3
             .arc()
             .innerRadius(innerRadius)
             .outerRadius(outerRadius);
-        //colors in order - anger,happy,insecure,neutral,sad,secure
-        const colors = d3.scaleOrdinal(['#b30000', '#b3b300', '#00b300', '#5a5a5a', '#0000b3', '#5a005a']);
 
-        const format = d3.format(".2f");
+        // adds associated colors based on which emotions are in data
+        const colorMap = {
+            anger: "#b30000",
+            happy: "#b3b300",
+            insecure: "#00b300",
+            neutral: "#5a5a5a",
+            sad: "#0000b3",
+            secure: "#5a005a",
+        };
+        
+        const colors = d3.scaleOrdinal(
+            data.map((obj) => colorMap[obj.emotion])
+        );
 
-        // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-        // let radius = Math.min(width, height) / 2 - margin;
-
-        // const svgElement = d3.select(svgRef.current);
-        // svgElement
-        //     .attr("width", width)
-        //     .attr("height", height)
-        //     .append("g")
-        //     .attr("transform", `translate(${width / 2}, ${height / 2})`);
-
-        //sorts emotions in data so that every speech data appears in the same order
-        data.sort(function (a, b) {
-            var emotionA = a.emotion.toUpperCase(); // ignore upper and lowercase
-            var emotionB = b.emotion.toUpperCase(); // ignore upper and lowercase
-            if (emotionA < emotionB) {
-                return -1;
-            }
-            if (emotionA > emotionB) {
-                return 1;
-            }
-
-            // names must be equal
-            return 0;
-        });
-        //console.log(data)
-
-        const div = d3.select(chartDivRef.current)
+        // TODO: tooltip popup
+        const div = d3
+            .select(chartDivRef.current)
             .append("div")
             .attr("class", "tooltip")
             .style("opacity", 0);
 
         const formatted_data = createPie(data);
         const group = d3.select(ref.current);
-        const groupWithData = group.selectAll("g.arc")
-            .data(formatted_data);
-
+        const groupWithData = group.selectAll("g.arc").data(formatted_data);
 
         groupWithData.exit().remove();
 
@@ -77,32 +68,27 @@ const PieChart = ({ data, title = "Pie Chart" }) => {
             .enter()
             .append("g")
             .attr("class", "arc")
-            .on('mouseover', function (d, i) {
-                d3.select(this).transition()
-                    .duration('50')
-                    .attr('opacity', '.5');
-                div.transition()
-                    .duration(50)
-                    .style("opacity", 1);
-                console.log(d)
-
+            .on("mouseover", function (d, i) {
+                d3.select(this)
+                    .transition()
+                    .duration("50")
+                    .attr("opacity", ".5");
+                div.transition().duration("50").style("opacity", 1);
             })
-            .on('mouseout', function (d, i) {
-                d3.select(this).transition()
-                    .duration('50')
-                    .attr('opacity', '1')
+            .on("mouseout", function (d, i) {
+                d3.select(this)
+                    .transition()
+                    .duration("50")
+                    .attr("opacity", "1");
             });
 
         const path = groupWithUpdate
             .append("path")
             .merge(groupWithData.select("path.arc"));
 
-
-
         path.attr("class", "arc")
             .attr("d", createArc)
-            .attr("fill", (d, i) => colors(i))
-
+            .attr("fill", (d, i) => colors(i));
 
         const text = groupWithUpdate
             .append("text")
@@ -113,17 +99,16 @@ const PieChart = ({ data, title = "Pie Chart" }) => {
             .attr("transform", (d) => `translate(${createArc.centroid(d)})`)
             .style("fill", "white")
             .style("font-size", 10)
-            .text((d) => format(d.value))
-
+            .text((d) => d.value);
     }, [data, title]);
 
     return (
         <>
             {/* <div ref={chartDivRef} /> */}
-            <svg width={450} height={300}>
+            <svg width={width} height={height}>
                 <g
                     ref={ref}
-                    transform={`translate(${100} ${100})`}
+                    transform={`translate(${width / 2} ${height / 2})`}
                 />
             </svg>
         </>
