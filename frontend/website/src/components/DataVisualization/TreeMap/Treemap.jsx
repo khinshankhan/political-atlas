9b071f50eaa09b995cc375cb2194057580ee23d4
Talha Rahman <39923265+TalhaR@users.noreply.github.com>
2021-05-02 12:38:29 -0400

    added tooltip on hover

--- frontend/website/src/components/DataVisualization/TreeMap/Treemap.jsx
+++ frontend/website/src/components/DataVisualization/TreeMap/Treemap.jsx
@@ -5,28 +5,31 @@ import * as d3 from "d3";
 import "./Treemap.css";
 
 import { emotionsMap } from "src/utils/emotions";
-import { arrToHex, contrastColor } from "src/utils/utils";
+import { arrToHex, contrastColor, roundDecimal2 } from "src/utils/utils";
 
 const TreeMap = ({ data, title = "Tree Map" }) => {
     const svgRef = useRef(null);
-    
-    useEffect(() => {
-        const svg = d3.select(svgRef.current);
+    const chartDivRef = useRef();
 
-        // conforming to D3 tree hierarchy 
+    useEffect(() => {
+        const div = d3.select(chartDivRef.current);
+        div.attr("class", "tooltip").style("opacity", 0);
+
+        const sum = d3.sum(data, ({ value }) => value);
+
+        // conforming data to D3 tree hierarchy
         const treeData = {
-            name: '',
+            name: "",
             children: [
                 {
-                    name: 'Emotions',
-                    children: data
-                }
-            ]
-        }
+                    name: "Emotions",
+                    children: data,
+                },
+            ],
+        };
 
         const width = 600;
         const height = 300;
-        svg.attr("width", width).attr("height", height)
 
         const root = d3
             .hierarchy(treeData)
@@ -35,26 +38,53 @@ const TreeMap = ({ data, title = "Tree Map" }) => {
 
         const treemapRoot = d3.treemap().size([width, height]).padding(1)(root);
 
-        const nodes = svg
-            .selectAll('g')
+        const svgElement = d3
+            .select(svgRef.current)
+            .attr("width", width)
+            .attr("height", height)
+            .selectAll("g")
             .data(treemapRoot.leaves())
-            .join('g')
-            .attr('transform', (d) => `translate(${d.x0},${d.y0})`);
+            .join("g")
+            .attr("transform", (d) => `translate(${d.x0},${d.y0})`);
 
-        nodes
-            .append('rect')
-            .attr('width', (d) => d.x1 - d.x0)
-            .attr('height', (d) => d.y1 - d.y0)
-            .attr('fill', ({ data: d }) => arrToHex(emotionsMap[d.emotion].color));
+        svgElement
+            .append("rect")
+            .attr("width", (d) => d.x1 - d.x0)
+            .attr("height", (d) => d.y1 - d.y0)
+            .attr("fill", ({ data: d }) =>
+                arrToHex(emotionsMap[d.emotion].color)
+            )
+            // HACK: this is all a bad hack, we need to refactor this later
+            .on("mouseover", (event, { data: d, value }) => {
+                div.transition().duration(200).style("opacity", 0.9);
+                div.html(
+                    `<b>Emotion:</b> ${d.emotion}` +
+                        `<br><b>Occurences:</b> ${value}` +
+                        `<br><b>Percentage:</b> ${roundDecimal2(
+                            (value / sum) * 100
+                        )}%`
+                )
+                    .style("left", event.pageX + 30 + "px")
+                    .style("top", event.pageY - 30 + "px");
+            })
+            .on("mousemove", (event) => {
+                div.style("left", event.pageX + 30 + "px").style(
+                    "top",
+                    event.pageY - 30 + "px"
+                );
+            })
+            .on("mouseout", (d) => {
+                div.transition().duration(500).style("opacity", 0);
+            });
 
         const fontSize = 12;
-
-        nodes
-            .append('text')
+        // adds text to treeMap elements
+        svgElement
+            .append("text")
             .text((d) => `${d.data.emotion} ${d.data.value}`)
-            .attr('font-size', `${fontSize}px`)
-            .attr('x', 3)
-            .attr('y', fontSize)
+            .attr("font-size", `${fontSize}px`)
+            .attr("x", 3)
+            .attr("y", fontSize)
             .style(
                 "fill",
                 // TODO: use value to increase or decrease darkness
@@ -64,7 +94,7 @@ const TreeMap = ({ data, title = "Tree Map" }) => {
 
     return (
         <>
-            {/* <div ref={chartDivRef} /> */}
+            <div ref={chartDivRef} />
             <svg ref={svgRef} />
         </>
     );
